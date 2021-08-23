2a78d4f0dee1ffddfc1f160dff8e48e51c32ecc5
Alec Fraser <fraal904@student.otago.ac.nz>
2021-08-23 15:08:55 +1200

    change html to user site to prep for admin site

--- www/user-site/index.php
+++ www/user-site/index.php
@@ -24,6 +24,24 @@
 
 </nav>
 
+<?php
+ 
+$db_host   = '192.168.2.12';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+$q = $pdo->query("SELECT * FROM papers");
+
+while($row = $q->fetch()){
+  echo "<tr><td>".$row["code"]."</td><td>".$row["name"]."</td></tr>\n";
+}
+
+?>
 
 
 
